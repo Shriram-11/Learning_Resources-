0cb912fd784eba04241b433dd34d88cee60d80d0
SHRIRAM <nshriram1326@gmail.com>
2022-11-23 19:04:41 +0530

    Files Added

---
+++ Java/TW1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class TW1 {
+    public static void main(String[] args) {
+        int[][] marks = new int[5][3];
+        int[] total = new int[5];
+        int[] avg = new int[5];
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Student" + (i + 1));
+            for (int j = 0; j < 3; j++) {
+                marks[i][j] = in.nextInt();
+                total[i] = marks[i][j];
+            }
+        }
+        System.out.println("--");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Student:" + (i + 1));
+            System.out.println("Total marks:" + total[i]);
+            System.out.println("Average:" + computeAvg(marks[i][0], marks[i][1], marks[i][2]));
+        }
+    }
+
+    static int computeAvg(int a, int b, int c) {
+        int min = a;
+        if (b < min)
+            min = b;
+        if (c < min)
+            min = c;
+        int sum = a + b + c - min;
+        return (int) Math.ceil(sum / 2.0);
+    }
+
+}
---
+++ Java/hwrld.java
@@ -0,0 +1,5 @@
+class hwrld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
