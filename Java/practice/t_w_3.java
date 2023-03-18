c6d27345aae34db9e812a3a5432e97e936ae6248
SHRIRAM <nshriram1326@gmail.com>
2023-03-18 12:59:44 +0530

    practicing java tws, python file added

---
+++ Java/practice/t_w_3.java
@@ -0,0 +1,24 @@
+package practice;
+
+/*
+ 3. Write a program to demonstrate the implementation of parameterized:
+a. Methods.
+b. Constructor.
+3.1) A certain small bank intends to automate few of its banking operations for its customers.
+Design a class by name mybankAccount to store the customer data having following details:
+1.accountNumber 2. acctType 3. Name 4. Address 5. accountBalance
+The class must have both default and parameterized constructors. Write appropriate method
+to compute interest accrued on accountBalance based on accountType and time in years.
+Assume 5% for S/B account 6.5% for RD account and 7.65 for FD account. Further, add two
+methods withdrawAmount/depositAmount with amount as input to withdraw and deposit
+respectively. The withdrawAmount method must report in-sufficient balance if
+accountBalance falls below Rs. 1000.
+TASK 1 : Build the class with appropriate member variables, constructors and methods.
+TASK 2 : Instantiate three objects of above type and perform different operations on the
+same.
+TASK 3 : Write a function to display all the three customer details in a tabular form with
+appropriate column headings.
+ */
+public class t_w_3 {
+
+}
