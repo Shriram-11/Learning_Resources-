810b32ed5d494653147e4df9aac49284d21eed64
SHRIRAM <nshriram1326@gmail.com>
2023-06-10 12:52:59 +0530

    numpy documentation

--- python programs/notes/testing.py
+++ python programs/notes/testing.py
@@ -1,2 +1,12 @@
-a=eval(input("Enter:"))
-print(a)
+#write a python program to create a list of n names and display them
+li=eval(input("Enter list:"))
+occ=int(input("Enter element to count its occrences and remove it:"))
+
+if occ not in li:
+    print("Element not in the list")
+else:
+    count=0
+    while occ in li:
+        count+=1
+        li.remove(occ)
+    print("Updated list:",li,"\nOccrences of ",occ,":",count)
