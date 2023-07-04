98021a96b2f163a5b48da165ae5069ea15a8a5e7
SHRIRAM <nshriram1326@gmail.com>
2023-07-04 10:21:03 +0530

    Text File Handling

--- notes/t1.py
+++ notes/t1.py
@@ -1,51 +1,17 @@
-#write a menudriven python program to implement the list of movies with follwing functions
-'''
-add, serach, display all,remove all occ a movie
-'''
+# Python Program To read n names from the user and write to a text file
+# The program should also read the names from the text file and display them
 
-movies=[]
-def add():
-    m=eval(input("Enter movie details [movies_name,year,rating]:"))
-    movies.append(m)
-def search():
-    m=input("Enter movie name:")
-    for a in movies:
-        if a[0].lower()==m.lower():
-            print("Present\n",a)
-            break
-
-    else:
-        print("not present")
-def  display():
-    if len(movies)==0:
-        print("No movies")
-        return
-    for m in movies:
-        print(m)
-
-def remove_occ():
-    m=input("Enter movie name:")
-    for a in movies:
-        if a[0].lower()==m.lower():
-            movies.remove(a)
-
-def main():
+with open('sample.txt', 'w',newline="") as f:
     while True:
-        choice=int(input('Choices are:\n1. Add   2. Search  3. Remove  4.  Display All   5. Exit\nEnter Choice:'))
-        if choice==1:
-            add()
-        elif choice==2:
-            search()
-        elif choice==3:
-            remove_occ()
-        elif choice==4:
-            display()
-        elif choice==5:
-            print("Bye!")
+        n=int(input("Enter a name:"))
+        f.write(n)
+        if input("Enter more? (y/n):").lower() != 'y':
             break
-        else:
-            print("Invalid Input")
+    f.close()
 
-if __name__=='__main__':
-    main()
+with open('sample.txt', 'r') as f:
+    print("Sample.txt content:")
+    li=f.readlines()
+    for i in li:
+        print(i)
 
