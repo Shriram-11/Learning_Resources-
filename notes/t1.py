ecd03c34b65e673e6c5f8f656343a4a7b56f781a
SHRIRAM <nshriram1326@gmail.com>
2023-07-04 14:30:06 +0530

    Text File Handling

--- notes/t1.py
+++ notes/t1.py
@@ -1,17 +1,15 @@
-# Python Program To read n names from the user and write to a text file
+# Python Program To read n names from the user store it in a list and write to a text file
 # The program should also read the names from the text file and display them
 
+names = []
+n= int(input("Enter No.:"))
+for i in range(n):
+    names.append(input("Enter Name:")+'\n')
 with open('sample.txt', 'w',newline="") as f:
-    while True:
-        n=int(input("Enter a name:"))
-        f.write(n)
-        if input("Enter more? (y/n):").lower() != 'y':
-            break
-    f.close()
+    f.writelines(names)
 
 with open('sample.txt', 'r') as f:
     print("Sample.txt content:")
     li=f.readlines()
     for i in li:
-        print(i)
-
+        print(i[:-1])
