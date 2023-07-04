1634f6850decf3cd6acbe777715e91fe789181c7
SHRIRAM <nshriram1326@gmail.com>
2023-07-04 09:03:07 +0530

    Monte Carlo

---
+++ notes/paths.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load the stock price dataset
+df = pd.read_csv('all_stocks_5yr.csv')  # Replace 'your_dataset.csv' with the path to your dataset
+
+# Select a specific stock
+stock_name = 'AAPL'  # Replace with the stock symbol you want to analyze
+stock_data = df[df['Name'] == stock_name].reset_index(drop=True)
+
+# Extract historical closing prices
+closing_prices = stock_data['close']
+
+# Calculate daily returns
+returns = np.log(closing_prices / closing_prices.shift(1)).dropna()
+
+# Calculate average daily return and standard deviation
+mu = returns.mean()
+sigma = returns.std()
+
+# Define parameters for the simulation
+simulations = 2
+forecast_horizon = 252  # Number of trading days in a year
+
+# Perform Monte Carlo simulation
+simulated_prices = pd.DataFrame()
+for i in range(simulations):
+    # Generate random daily returns based on mean and standard deviation
+    daily_returns = np.random.normal(mu, sigma, forecast_horizon)
+    
+    # Calculate the simulated price path
+    price_path = closing_prices.iloc[-1] * np.exp(np.cumsum(daily_returns))
+    
+    # Append the simulated price path to the DataFrame
+    simulated_prices[i] = price_path
+
+
+
+# Plot the historical prices and simulated price paths
+# Plotting the simulated price paths
+plt.figure(figsize=(10, 6))
+for i in range(simulations):
+    plt.plot(simulated_prices[i], alpha=0.3)
+
+plt.xlabel("Time")
+plt.ylabel("Price")
+plt.title("Simulated Stock Price Paths")
+plt.show()
--- notes/testing.py
+++ notes/testing.py
@@ -1,12 +1,27 @@
-#write a python program to create a list of n names and display them
-li=eval(input("Enter list:"))
-occ=int(input("Enter element to count its occrences and remove it:"))
+import random
 
-if occ not in li:
-    print("Element not in the list")
-else:
-    count=0
-    while occ in li:
-        count+=1
-        li.remove(occ)
-    print("Updated list:",li,"\nOccrences of ",occ,":",count)
+def estimate_pi(num_points):
+    points_inside_circle = 0
+    points_inside_square = 0
+
+    for _ in range(num_points):
+        # Generate random x and y coordinates within the square [-1, 1] x [-1, 1]
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+
+        # Check if the point is inside the unit circle
+        if x**2 + y**2 <= 1:
+            points_inside_circle += 1
+
+        points_inside_square += 1
+
+    # Estimate pi using the ratio of points inside the circle to the total points inside the square
+    pi_estimate = 4 * (points_inside_circle / points_inside_square)
+    return pi_estimate
+
+# Perform the Monte Carlo simulation to estimate pi
+num_simulations = 10000000
+
+estimated_pi = estimate_pi(num_simulations)
+
+print("Estimated value of π:", estimated_pi)
