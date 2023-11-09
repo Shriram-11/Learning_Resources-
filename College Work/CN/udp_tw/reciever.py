e6d7d8bb711b22fd014223e68c4eae3d30b8edc9
SHRIRAM <nshriram1326@gmail.com>
2023-11-09 10:30:29 +0530

    udp

---
+++ College Work/CN/udp_tw/reciever.py
@@ -0,0 +1,16 @@
+
+import socket
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to a specific address and port
+server_address = ('localhost', 12345)  
+sock.bind(server_address)
+
+while True:
+    print('\nWaiting to receive message')
+    data, address = sock.recvfrom(4096)
+
+    print(f'Received {len(data)} bytes from {address}')
+    print(data.decode())
---
+++ College Work/CN/udp_tw/sender.py
@@ -0,0 +1,25 @@
+#UDP Client
+import socket
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Define the server (localhost) and port number
+server_address = ('localhost', 12345)
+
+try:
+    # Define your message
+    while True:
+        message = input('Enter message:')
+
+        # Send the message
+        print(f'Sending "{message}"')
+        sent = sock.sendto(message.encode(), server_address)
+
+except Exception as e:
+    print(f'An error occurred: {e}')
+
+finally:
+    # Close the socket
+    print('Closing socket')
+    sock.close()
