0d7e8e6f85ae163e6fbe33eb306fdb4ef5363f70
SHRIRAM <nshriram1326@gmail.com>
2023-11-23 10:20:15 +0530

    bfa complete

--- College Work/CN/Distance_Vector_Fouting/bfa.py
+++ College Work/CN/Distance_Vector_Fouting/bfa.py
@@ -24,8 +24,38 @@ class Network:
 
     def update_distance_vector(self, node):
         for dest in self.nodes:
-            pass
+            if dest != node:
+                min_cost = sys.maxsize
+                for neighbour in self.graph[node]:
+                    if dest in self.distance_vector[neighbour]:
+                        cost = self.distance_vector[neighbour][dest] + \
+                            self.graph[node][neighbour]
+                        min_cost = min(min_cost, cost)
+                self.distance_vector[node][dest] = min_cost
+
+    def print_routing_table(self, node):
+
+        print(f"Routing table for Node:{node}\nDestination\t\tCost")
+        for dest, cost in self.distance_vector[node].items():
+            if dest != node:
+                print(f"{dest}\t\t{cost}\n")
 
 
 if __name__ == '__main__':
     nodes = [1, 2, 3, 4, 5]
+    net = Network(nodes)
+    net.add_link(1, 2, 2)
+    net.add_link(1, 3, 2)
+    net.add_link(1, 4, 1)
+    net.add_link(2, 3, 3)
+    net.add_link(2, 5, 1)
+    net.add_link(3, 4, 4)
+    net.add_link(3, 5, 1)
+
+    for node in nodes:
+        net.initialize_distance_vector(node)
+    for _ in range(6):
+        for node in nodes:
+            net.update_distance_vector(node)
+    for node in nodes:
+        net.print_routing_table(node)
